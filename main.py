1175c2268a9d93e2751f4820f2c2ddfad7db043a
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-11-03 20:58:20 +0100

    Basic sentence breakdown implemented

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ morfeusz = morfeusz2.Morfeusz()
 
 analysis = morfeusz.analyse(sentence)
 
-for i, j, interpretation in analysis:
-  print(i, j, interpretation)
+sentenceWords = []
 
+prev_i = None
+
+for i, j, interpretation in analysis:
+    print(i, j, interpretation)
+    word = interpretation[1].split(':')[0]
+
+    if i != prev_i:
+        sentenceWords.append(word)
+    prev_i = i
+
+print(sentenceWords)
