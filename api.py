0880dc6baf1d30dfa97fe652c732e29b2edaa249
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-11-10 00:13:30 +0100

    Initial api.py script using Flask added

---
+++ api.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from convert_to_base_words import convert_to_base_words
+from read_sentences import read_sentences
+
+app = Flask(__name__)
+
+@app.route('/')
+def output_sentences():
+    sentences = read_sentences("input.txt")
+
+    return jsonify([convert_to_base_words(sentence) for sentence in sentences])
