c18b3085377730f2782d34736cb3f06959dc02c0
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-11-12 23:34:42 +0100

    Meaningless words dropping added

--- convert_to_base_words.py
+++ convert_to_base_words.py
@@ -2,6 +2,14 @@ import morfeusz2
 
 morfeusz = morfeusz2.Morfeusz()
 
+word_types_to_drop = [
+    'interj',
+    'interp',
+    'part',
+    'prep',
+    'brev'
+]
+
 
 def convert_to_base_words(sentence):
     analysis = morfeusz.analyse(sentence)
@@ -13,9 +21,13 @@ def convert_to_base_words(sentence):
     for i, j, interpretation in analysis:
         # TODO: find out which base form take
         word = interpretation[1].split(':')[0]
+        word_type = interpretation[2].split(':')[0]
 
-        if i != prev_i:
+        is_word_allowed = word_type not in word_types_to_drop
+
+        if i != prev_i and is_word_allowed:
             sentenceWords.append(word)
+
         prev_i = i
 
     return sentenceWords
