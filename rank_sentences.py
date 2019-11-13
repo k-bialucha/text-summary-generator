1c76c562b9a7114f178165295faef304eb23d377
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-11-13 01:47:04 +0100

    Sentence ranking normalization applied

--- rank_sentences.py
+++ rank_sentences.py
@@ -6,7 +6,9 @@ def rank_sentences(base_form_sentences, word_rank):
         for base_word in sentence_base_words:
             ranking += word_rank.get(base_word)
 
-        ranked_pair = (sentence_base_words, ranking)
+        ranking_normalized = ranking / len(sentence_base_words)
+
+        ranked_pair = (sentence_base_words, ranking_normalized)
         sentences_ranked.append(ranked_pair)
 
     return sentences_ranked
