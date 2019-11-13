18d67de8e6f09877973cd1c6705a5367057aa537
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-11-13 01:50:31 +0100

    Summary generation handled in the main module

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import morfeusz2
+
 from read_sentences import read_sentences
 from convert_to_base_words import convert_to_base_words
 from create_word_ranking import create_word_ranking
 from rank_sentences import rank_sentences
+from enhance_original_sentences import enhance_original_sentences
+from create_summary import create_summary
+from write_sentences import write_sentences
 
 sentences = read_sentences("input.txt")
 
@@ -15,6 +19,12 @@ for sentence in sentences:
 word_ranking = create_word_ranking(base_form_sentences)
 sentences_ranked = rank_sentences(base_form_sentences, word_ranking)
 
-print(base_form_sentences)
-print(word_ranking)
-print(sentences_ranked)
+original_sentences_ranked = enhance_original_sentences(
+    sentences, sentences_ranked)
+
+summary_sentences = create_summary(original_sentences_ranked, 0.5)
+
+write_sentences("output.txt", summary_sentences)
+
+print('SUMMARY')
+print(summary_sentences)
