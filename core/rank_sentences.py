921f750e19820b86198fc3364570962848060df0
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-01-19 22:47:01 +0100

    Sentence shuffling added in the rank_sentences function

--- core/rank_sentences.py
+++ core/rank_sentences.py
@@ -2,27 +2,35 @@ from math import floor
 import random
 
 
+def sortByOriginalOrder(val):
+    return val[2]
+
+
 def rank_sentences(base_form_sentences, word_ranking, ranking_aging):
     sentences_count = len(base_form_sentences)
 
     indices = list(range(0, sentences_count))
 
-    random.shuffle(indices)
+    # shuffle original indices
+    # to get more fair ranking
+    random.Random(999).shuffle(indices)
 
     sentences_ranked = []
     for index in indices:
         sentence_base_words = base_form_sentences[index]
-        ranking = 0
+        ranking = 0.0
 
         for base_word in sentence_base_words:
-            ranking += word_ranking.get(base_word)
-
+            ranking += floor(word_ranking[base_word])
             word_ranking[base_word] = floor(
                 word_ranking[base_word] * (100 - ranking_aging)) / 100
 
         ranking_normalized = ranking / len(sentence_base_words)
 
-        ranked_pair = (sentence_base_words, ranking_normalized)
-        sentences_ranked.append(ranked_pair)
+        ranked_tuple = (sentence_base_words, ranking_normalized, index)
+        sentences_ranked.append(ranked_tuple)
+
+    # restore original order of sentences
+    sentences_ranked.sort(key=sortByOriginalOrder)
 
     return sentences_ranked
