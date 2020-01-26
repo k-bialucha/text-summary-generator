e8a4e84ab678d29b1eeeb6633b934cea586299e8
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-01-26 13:39:10 +0100

    Api module change to use SummaryGenerator class

--- api.py
+++ api.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify
 
-from core.summarize import summarize
+from core.generator import SummaryGenerator
 from read_sentences import read_sentences
 
 
 app = Flask(__name__)
+
+generator = SummaryGenerator()
 
 
 @app.route('/')
 def output_sentences():
     sentences = read_sentences("input.txt")
 
-    summary_sentences = summarize(sentences)
+    # TODO: add SummaryGenerator config support
+    summary_sentences = generator.summarize(sentences)
+
     return jsonify(summary_sentences)
