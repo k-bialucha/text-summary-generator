e17b3e42769a78cfc9a486032bb432f05c85d541
Kamil Bialucha <kamil.bialucha@gmail.com>
2020-01-26 13:24:29 +0100

    Set_output module reimplemented using SummaryGenerator; summarize module removed

--- set_output.py
+++ set_output.py
@@ -1,16 +1,23 @@
-from core.summarize import summarize
 import tkinter as tk
+from core.generator import SummaryGenerator
 
 
-def set_output(output, text, percent, aging, start_range, start_weight, end_range, end_weight, debug, ignore, use_special_words):
-    lines = text.splitlines()
-    sentences = []
+def set_output(output, text, percent, aging, start_range, start_weight, end_range, end_weight, debug, ignored_words, use_special_words):
+    config = {
+        "summary_percent": percent,
+        "ranking_aging": aging,
+        "segment_boost": [(0, start_range, start_weight), (100 - end_range, 100, end_weight)],
+        "debug": debug,
+        "ignored_words": ignored_words,
+        "use_special_words": use_special_words
+    }
 
-    for sentence in lines:
-        sentences.append(sentence)
+    generator = SummaryGenerator()
+    generator.config = config
 
-    summary_sentences = summarize(
-        sentences, percent, aging, start_range, start_weight, end_range, end_weight, debug == 1, ignore, use_special_words == 1)
+    sentences = text.splitlines()
+
+    summary_sentences = generator.summarize(sentences)
     summary = '\n'.join(summary_sentences)
 
     output.config(state=tk.NORMAL)
